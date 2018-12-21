1fca2fd081f7f0eae1e7551ef56391ce7377344e
Josh Close <narshe@gmail.com>
2018-12-20 21:45:46 -0600

    wip

--- docs-src/src/pages/documentation.js
+++ docs-src/src/pages/documentation.js
@@ -4,12 +4,42 @@ import Content from "../components/content";
 import TableOfContents from "../components/table-of-contents";
 
 export default class Documentation extends Component {
+
+	state = {
+		sidebarBottom: 0
+	}
+
+	componentDidMount() {
+		window.addEventListener("scroll", this.handleWindowScroll);
+
+		this.footerEl = document.querySelector("#root > footer");
+
+		this.updateSidebarBottom();
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("scroll", this.handleWindowScroll);
+	}
+
+	updateSidebarBottom() {
+		const visibleHeight = window.innerHeight - this.footerEl.getBoundingClientRect().top;
+		const sidebarBottom = Math.max(0, visibleHeight);
+
+		this.setState({ sidebarBottom });
+	}
+
+	handleWindowScroll = () => {
+		this.updateSidebarBottom();
+	}
+
 	render() {
+		const { sidebarBottom } = this.state;
+
 		return (
 			<div className="documentation container is-fluid">
 				<div className="columns">
 					<div className="column container-right">
-						<div className="sidebar">
+						<div className="sidebar" style={{ bottom: sidebarBottom }}>
 							<TableOfContents />
 						</div>
 					</div>
